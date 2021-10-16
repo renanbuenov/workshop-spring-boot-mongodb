54deb7efd217cbe810f2490426ba8687b59f3db4
renanbuenov <renanbuenov@hotmail.com>
2021-10-16 18:17:22 -0300

    Obtaining a post by id

---
+++ src/main/java/com/renanbueno/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.renanbueno.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.renanbueno.workshopmongo.domain.Post;
+import com.renanbueno.workshopmongo.repository.PostRepository;
+import com.renanbueno.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Optional<Post> findById(String id) {
+		Optional<Post> user = repo.findById(id);
+		if (user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user;
+	}
+}
